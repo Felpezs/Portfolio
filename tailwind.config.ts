644f55164e45c8090870638a679f129dff243962
Felpezs <felipe.edfreire@gmail.com>
2024-02-14 18:06:28 -0300

    feat: creating theme for the application

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,42 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    spacing: {
+      1: "20px",
+      2: "40px",
+      3: "60px",
+      4: "80px",
+    },
+    colors: {
+      white: "#ffff",
+      surface: {
+        800: "#070e18",
+        600: "#0c1727",
+        500: "#0d192b",
+      },
+      primary: {
+        700: "#1a4c99",
+        500: "#246bd7",
+      },
+      secondary: {
+        900: "#300f5a",
+        800: "#3f1476",
+        600: "#6821c4",
+        500: "#7224d7",
+        300: "#a16ce4",
+        200: "#be9aed",
+        50: "#f1e9fb",
+      },
+      neutral: {
+        700: "#8d8d8d",
+        600: "#b4b4b4",
+        500: "#c6c6c6",
+        100: "#ededed",
+        50: "#f9f9f9",
       },
     },
   },
