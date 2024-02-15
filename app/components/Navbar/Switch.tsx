38c6ec52d0dd06322d529d39deb013b83a5dbc05
Felpezs <felipe.edfreire@gmail.com>
2024-02-14 21:07:50 -0300

    feat: switch component basic structure

---
+++ app/components/Navbar/Switch.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Switch = () => {
+  return (
+    <div className="h-[24px] w-[52px] py-[2px] px-[4px] bg-secondary-800 rounded-full flex justify-end">
+      <div className="h-1 w-1 bg-white rounded-full"></div>
+    </div>
+  );
+};
+
+export default Switch;
