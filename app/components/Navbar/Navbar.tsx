011bb0e68bc846be74b5fee92cd8ea22d9522aa2
Felpezs <felipe.edfreire@gmail.com>
2024-02-14 21:08:11 -0300

    feat: navbar base structure

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import BrazilFlag from "../icons/BrazilFlag";
+import UsFlag from "../icons/UsFlag";
+import SunIcon from "../icons/SunIcon";
+import MoonIcon from "../icons/MoonIcon";
+import Switch from "./Switch";
+
+const Navbar = () => {
+  return (
+    <nav className="dark:text-secondary-50 px-3 py-1 border-b-[3px] border-secondary-900 flex flex-row">
+      <ul className="flex flex-row gap-1 mr-auto">
+        {[
+          ["Home", "#"],
+          ["About", "#"],
+          ["Experience", "#"],
+          ["Projects", "#"],
+          ["Contact", "#"],
+        ].map(([title, url], index) => {
+          return (
+            <li key={index}>
+              <a className="hover:text-secondary-200" href={url}>
+                {title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="flex gap-1">
+        <div className="flex gap-[10px]">
+          <BrazilFlag />
+          <UsFlag />
+        </div>
+        <div className="flex gap-[10px]">
+          <SunIcon />
+          <Switch />
+          <MoonIcon />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar/Navbar";
+
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col">
+      <Navbar />
       <h1 className="text-white">Hello World</h1>
     </main>
   );
