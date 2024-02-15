de94fe0ddb2ef446006780014b544f38cb23f935
Felpezs <felipe.edfreire@gmail.com>
2024-02-15 13:12:16 -0300

    feat: first content area

---
+++ app/components/icons/ArrowIcon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ArrowIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="36"
+      height="37"
+      fill="none"
+      viewBox="0 0 36 37"
+    >
+      <path
+        fill="#ffff"
+        d="M18 .5a18 18 0 1018 18 18.02 18.02 0 00-18-18zm6.853 20.028L18.98 26.4a1.389 1.389 0 01-1.962 0l-5.872-5.872a1.385 1.385 0 011.958-1.958l3.51 3.51V11.578a1.385 1.385 0 012.77 0V22.08l3.51-3.51a1.385 1.385 0 011.958 1.957z"
+      ></path>
+    </svg>
+  );
+};
+
+export default ArrowIcon;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 import Navbar from "./components/Navbar/Navbar";
+import ArrowIcon from "./components/icons/ArrowIcon";
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
+    <main>
       <Navbar />
-      <h1 className="text-white">Hello World</h1>
+      <div className="grid grid-cols-12 gap-x-1 mx-3">
+        <section className="col-span-full gap-y-1 my-5 px-[16.89%]">
+          <h3 className="text-neutral-600 mb-1">Hello, I am</h3>
+          <h1 className="text-white mb-1">Felipe Eduardo</h1>
+          <h1 className="text-white mb-1">I&apos;m a software developer</h1>
+          <h3 className="text-neutral-600 mb-1 text-balance">
+            I’m a developer specialized in the development of user interfaces,
+            providing great experiences for the users.
+          </h3>
+          <div className="flex justify-center py-1">
+            <ArrowIcon />
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
