a54d6fe0dd268803abe43d03cfc497ac797c110f
Felpezs <felipe.edfreire@gmail.com>
2024-02-17 20:36:34 -0300

    feat: Chip props

--- app/components/Chip.tsx
+++ app/components/Chip.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-type Props = {
+export type ChipProps = {
   children: string;
 };
 
-const Chip = ({ children }: Props) => {
+const Chip: React.FC<ChipProps> = ({ children }) => {
   return (
     <div className="w-fit h-[20px] bg-secondary-200 rounded-full px-[16px] border-[2px] border-secondary-300 flex items-center">
       <p className="text-xs text-black font-medium">{children}</p>
