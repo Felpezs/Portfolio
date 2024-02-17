9ab76008f1ca906559248565718e87d00607b79c
Felpezs <felipe.edfreire@gmail.com>
2024-02-17 20:39:00 -0300

    feat: mapping jobs and stacks

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-"use client";
-
 import Image from "next/image";
-import Navbar from "./components/Navbar/Navbar";
-import ArrowIcon from "./components/icons/ArrowIcon";
-import Button from "./components/Button";
+import Navbar from "@/app/components/Navbar/Navbar";
+import ArrowIcon from "@/app/components/icons/ArrowIcon";
+import About from "@/app/components/About";
+import { jobs } from "@/json/jobs.json";
+import JobCard from "@/app/components/JobCard";
+import Chip from "@/app/components/Chip";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <div className="grid grid-cols-12 auto-cols-max gap-x-1 mx-3">
-        <section className="col-span-full gap-y-1 my-5 px-[16.89%]">
+      <div className="grid grid-cols-12 auto-cols-max gap-x-1 gap-y-5 mx-3">
+        <section className="col-span-full gap-y-1 mt-5 px-[16.89%]">
           <h3 className="text-neutral-600 mb-1">Hello, I am</h3>
           <h1 className="text-white mb-1">Felipe Eduardo</h1>
           <h1 className="text-white mb-1">I&apos;m a software developer</h1>
@@ -31,27 +32,22 @@ export default function Home() {
             className="m-auto"
           />
         </section>
-        <section className="col-span-7 flex flex-col gap-2">
-          <h2 className="text-white">About me</h2>
-          <p className="text-neutral-500 text-lg">
-            Hello, I&apos;m Felipe Eduardo, a Frontend developer. I hold a
-            Bachelor&apos;s degree in Information Systems from the School of
-            Technology - UNICAMP. With over 4 years of experience in
-            development, I am currently focused on creating exceptional
-            interfaces for global users.
-          </p>
-          <p className="text-neutral-500 text-lg">
-            I am passionate about assisting individuals in solving their
-            problems and continuously exploring new technologies and
-            methodologies to address challenges.
-          </p>
-          <p className="text-neutral-500 text-lg">
-            What truly motivates me is the realization that my expertise is
-            making a positive impact on others&apos; lives!
-          </p>
-          <Button variant="primary" onClick={() => {}} className="ml-auto">
-            Check my Projects
-          </Button>
+        <About />
+        <section className="col-span-full">
+          <h2 className="text-white text-center mb-2">Experiences</h2>
+          {jobs.map((job) => (
+            <JobCard
+              key={job.index}
+              company={job.company}
+              description={job.description}
+              fromDate={job.fromDate}
+              toDate={job.toDate}
+            >
+              {job.technologies.map((technology, index) => (
+                <Chip key={index}>{technology}</Chip>
+              ))}
+            </JobCard>
+          ))}
         </section>
       </div>
     </main>
