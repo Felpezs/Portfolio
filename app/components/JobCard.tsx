8fc6690bf7684ffa34f084ddcc9383736f643f83
Felpezs <felipe.edfreire@gmail.com>
2024-02-17 23:35:31 -0300

    feat: changing state between active card

--- app/components/JobCard.tsx
+++ app/components/JobCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {
+import React, {
   forwardRef,
   useImperativeHandle,
   useState,
@@ -14,24 +14,25 @@ type JobCardProps = {
   toDate: string;
   company: string;
   description: string;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
   children?: React.ReactElement<ChipProps> | React.ReactElement<ChipProps>[];
 };
 
-type Ref = {
+export type JobCardRef = {
   setIsActive: Dispatch<SetStateAction<boolean>>;
 };
 
-const JobCard = forwardRef<Ref, JobCardProps>(function JobCard(
-  { fromDate, toDate, company, description, children },
+const JobCard = forwardRef<JobCardRef | null, JobCardProps>(function JobCard(
+  { fromDate, toDate, company, description, onClick, children },
   ref
 ) {
   const [isActive, setIsActive] = useState<boolean>(false);
 
   const deactivatedHr = (
-    <hr className="h-auto border-none w-[2px] text-neutral-700 bg-neutral-700" />
+    <hr className="h-full border-none w-[2px] text-neutral-700 bg-neutral-700 mx-auto" />
   );
   const activatedHr = (
-    <hr className="h-auto border-none w-[4px] text-white bg-white" />
+    <hr className="h-full border-none w-[4px] text-white bg-white" />
   );
 
   useImperativeHandle(
@@ -49,8 +50,10 @@ const JobCard = forwardRef<Ref, JobCardProps>(function JobCard(
       <p className="text-base text-neutral-600 min-w-[120px] text-right">
         {fromDate}-{toDate}
       </p>
-      {isActive ? activatedHr : deactivatedHr}
-      <div>
+      <div className="h-auto min-w-[4px]">
+        {isActive ? activatedHr : deactivatedHr}
+      </div>
+      <div onClick={onClick} className="cursor-pointer">
         <h3 className="text-white mb-1">{company}</h3>
         <p className="text-neutral-500 mb-1">{description}</p>
         <div className="flex gap-[10px] mb-1 flex-wrap">{children}</div>
