f6e08c97baab288a0efbeb5cb2de0976af6da9c2
Felpezs <felipe.edfreire@gmail.com>
2024-03-06 19:40:25 -0300

    feat: handling index change

--- app/components/Carousel/Carousel.tsx
+++ app/components/Carousel/Carousel.tsx
@@ -1,16 +1,27 @@
-import React, { Children, ReactElement, useState } from "react";
+import React, { Children, ReactElement, useEffect, useState } from "react";
 import { CarouselItemProps } from "./CarouselItem";
+import Arrow from "../icons/Arrow";
+import DotIndicator from "../icons/DotIndicator";
 
 type CarouselProps = {
   children:
     | React.ReactElement<CarouselItemProps>
     | React.ReactElement<CarouselItemProps>[];
+  className?: React.HTMLAttributes<HTMLDivElement>["className"];
 };
 
-const Carousel = ({ children }: CarouselProps) => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-
+const Carousel = ({ children, className }: CarouselProps) => {
   const carouselItems = Children.toArray(children);
+
+  const middleOfCarouselItems = Math.floor(carouselItems.length / 2);
+
+  const [currentIndex, setCurrentIndex] = useState<number>(
+    middleOfCarouselItems
+  );
+
+  useEffect(() => {
+    console.log("Current index:" + currentIndex);
+  }, [currentIndex]);
 
   const prevIndex =
     (currentIndex - 1 + carouselItems.length) % carouselItems.length;
@@ -24,27 +35,48 @@ const Carousel = ({ children }: CarouselProps) => {
     return indexes.map((index) => carouselItems[index]);
   };
 
-  const handleClick = (index: number) => {
-    setCurrentIndex(index);
+  const handleClickPrev = () => {
+    setCurrentIndex((prevState) => prevState - 1);
   };
-  const a = carouselItems[0] as ReactElement<CarouselItemProps>;
+
+  const handleClickNext = () => {
+    setCurrentIndex((prevState) => prevState + 1);
+  };
 
   return (
-    <div className="flex justify-center items-center transition transform duration-[5s] min-h-[275px] flex-shrink-0">
-      {getCarouselItemsToShow().map((item, index) => {
-        return React.cloneElement(
-          item as React.ReactElement<CarouselItemProps>,
-          {
-            className:
-              "first:w-[297px] last:w-[297px] " +
-              `${
-                index === 1
-                  ? "absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-auto h-auto"
-                  : ""
-              }`,
-          }
-        );
-      })}
+    <div className={className}>
+      <div className="flex justify-center items-center transition transform duration-[5s] h-[279px] flex-shrink-0 mb-[20px]">
+        {getCarouselItemsToShow().map((item, index) => {
+          return React.cloneElement(
+            item as React.ReactElement<CarouselItemProps>,
+            {
+              className:
+                "first:w-[297px] last:w-[297px] " +
+                `${
+                  index === 1
+                    ? "absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"
+                    : ""
+                }`,
+            }
+          );
+        })}
+      </div>
+      <div className="flex items-center gap-[10px] justify-center h-[46px]">
+        <Arrow onClick={handleClickPrev} />
+        {carouselItems.map((carouselItem, index) => (
+          <DotIndicator
+            key={(carouselItem as React.ReactElement<CarouselItemProps>).key}
+            isActive={index === 0}
+          />
+        ))}
+        <Arrow rotate onClick={handleClickNext} />
+      </div>
+      <p className="text-neutral-500 mt-1">
+        {
+          (carouselItems[currentIndex] as React.ReactElement<CarouselItemProps>)
+            .props.description
+        }
+      </p>
     </div>
   );
 };
--- app/components/Carousel/CarouselItem.tsx
+++ app/components/Carousel/CarouselItem.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 export type CarouselItemProps = {
   imgSrc: string;
+  description: string;
   className?: React.HTMLAttributes<HTMLDivElement>["className"];
 };
 
