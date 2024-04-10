f4c95fffdaad28cae3c8742d6d70f0ca7b5a43af
felipe <felipe.edfreire@gmail.com>
2024-04-10 09:47:37 -0300

    feat: navbar icons

---
+++ app/components/icons/Close.tsx
@@ -0,0 +1,18 @@
+const Close = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={32}
+      viewBox="0 -960 960 960"
+      width={32}
+      className="dark:text-white"
+    >
+      <path
+        fill="currentColor"
+        d="M256-200l-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224z"
+      />
+    </svg>
+  );
+};
+
+export default Close;
---
+++ app/components/icons/Menu.tsx
@@ -0,0 +1,18 @@
+const Menu = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={32}
+      viewBox="0 -960 960 960"
+      width={32}
+      className="dark:text-white"
+    >
+      <path
+        fill="currentColor"
+        d="M120-240v-80h720v80H120zm0-200v-80h720v80H120zm0-200v-80h720v80H120z"
+      />
+    </svg>
+  );
+};
+
+export default Menu;
