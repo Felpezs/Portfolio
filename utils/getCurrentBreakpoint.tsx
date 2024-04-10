f6ec9d324d636771549118737c1ad395262488af
felipe <felipe.edfreire@gmail.com>
2024-04-10 19:25:47 -0300

    feat: using react responsive instead of watching resive event

--- utils/getCurrentBreakpoint.tsx
+++ utils/getCurrentBreakpoint.tsx
@@ -9,23 +9,6 @@ export const getBreakpointValue = (value: Screens): number =>
   parseInt(
     fullConfig.theme.screens[value].slice(
       0,
-      fullConfig.theme.screens[value].indexOf("px")
-    )
+      fullConfig.theme.screens[value].indexOf("px"),
+    ),
   );
-
-export const getCurrentBreakpoint = (): string => {
-  let currentBreakpoint: string = "";
-  let biggestBreakpointValue = 0;
-
-  for (const breakpoint of Object.keys(fullConfig.theme.screens)) {
-    const breakpointValue = getBreakpointValue(breakpoint as Screens);
-    if (
-      breakpointValue > biggestBreakpointValue &&
-      window.innerWidth >= breakpointValue
-    ) {
-      biggestBreakpointValue = breakpointValue;
-      currentBreakpoint = breakpoint as Screens;
-    }
-  }
-  return currentBreakpoint;
-};
