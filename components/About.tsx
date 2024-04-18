bdb2de1ed52ef7e42af696ec2232d69a7fee7964
felipe <felipe.edfreire@gmail.com>
2024-04-18 20:41:20 -0300

    refactor: font sizes

--- components/About.tsx
+++ components/About.tsx
@@ -4,20 +4,20 @@ import Button from "./Button";
 const About = () => {
   return (
     <>
-      <h2 className="text-white md:text-2xl lg:text-4xl">About me</h2>
-      <p className="text-balance text-base text-neutral-500 md:text-xl">
+      <h2 className="text-3xl text-white lg:text-4xl">About me</h2>
+      <p className="text-balance text-base text-neutral-500 md:text-lg">
         Hello, I&apos;m Felipe Eduardo, a Frontend developer. I hold a
         Bachelor&apos;s degree in Information Systems from the School of
         Technology - UNICAMP. With over 4 years of experience in development, I
         am currently focused on creating exceptional interfaces for global
         users.
       </p>
-      <p className="text-balance text-base text-neutral-500 md:text-xl">
+      <p className="text-balance text-base text-neutral-500 md:text-lg">
         I am passionate about assisting individuals in solving their problems
         and continuously exploring new technologies and methodologies to address
         challenges.
       </p>
-      <p className="text-balance text-base text-neutral-500 md:text-xl">
+      <p className="text-balance text-base text-neutral-500 md:text-lg">
         What truly motivates me is the realization that my expertise is making a
         positive impact on others&apos; lives!
       </p>
