a3f2299e071b6e440b852d583e3d1371907917e8
felipe <felipe.edfreire@gmail.com>
2024-04-18 21:20:02 -0300

    feat: responsive experiences area

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -9,14 +9,14 @@ const Experiences = () => {
 
   return (
     <>
-      <h2 className="col-span-full mb-2 text-3xl text-white lg:text-4xl">
+      <h2 className="col-span-full mb-2 text-center text-3xl text-white md:text-left lg:text-4xl">
         Experiences
       </h2>
-      <ul className="col-span-1 h-fit rounded-md bg-surface-600 text-lg text-secondary-50 sm:col-span-3">
+      <ul className="col-span-full mb-2 h-fit overflow-hidden rounded-md bg-surface-600 text-base text-secondary-50 md:col-span-3 md:mb-[0px] lg:col-span-4 lg:max-w-[300px]">
         {jobs.map((job) => (
           <li
             key={job.index}
-            className={`cursor-pointer border-l-4 p-1 transition-colors  ${activeJob == job.index ? "border-secondary-300 text-secondary-200" : "border-transparent hover:border-secondary-300 hover:text-secondary-200"}`}
+            className={`cursor-pointer  border-l-4 px-2 py-1 transition-colors  ${activeJob == job.index ? "border-secondary-300 text-secondary-200" : "border-transparent hover:border-secondary-300 hover:text-secondary-200"}`}
             onClick={() => {
               setActiveJob(job.index);
             }}
@@ -25,13 +25,19 @@ const Experiences = () => {
           </li>
         ))}
       </ul>
-      <div className="col-span-9 min-h-[300px] text-white">
+      <div className="col-span-full min-h-[300px] text-white md:col-span-5 lg:col-span-8">
         <div className="mb-1 flex items-center justify-between">
-          <h3 className="text-2xl">{jobs[activeJob].role}</h3>
-          <span className="text-neutral-300">{jobs[activeJob].date}</span>
+          <h3 className="text-balance pr-1 text-lg lg:text-2xl">
+            {jobs[activeJob].role}
+          </h3>
+          <span className="text-base text-neutral-300">
+            {jobs[activeJob].date}
+          </span>
         </div>
-        <h3 className="mb-1 text-secondary-300">{jobs[activeJob].company}</h3>
-        <p className="mb-1 text-lg text-neutral-500">
+        <h3 className="mb-1 text-lg text-secondary-300">
+          {jobs[activeJob].company}
+        </h3>
+        <p className="mb-1 text-base text-neutral-500 lg:text-lg">
           {jobs[activeJob].description}
         </p>
         <div className="flex flex-wrap gap-[10px]">
