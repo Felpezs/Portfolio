0cffab1fcde622363e5647efdb3b8bdd0b5bcfe2
felipe <felipe.edfreire@gmail.com>
2024-04-20 17:54:33 -0300

    fix: download resume button on navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -49,9 +49,9 @@ const Navbar = () => {
       </span>
 
       <ul
-        className={`absolute left-[0px] top-[0px] flex h-[100vh] flex-col items-center justify-center bg-surface-700 text-[18px] transition-width duration-700 ease-in-out ${
-          showMenu ? "w-[250px] " : "w-[0px]"
-        } basis-[484px] gap-[20px] md:static md:mr-auto md:flex md:h-auto md:w-auto md:flex-row md:bg-transparent md:text-base md:transition-none`}
+        className={`${transition ? "transition-transform duration-700 ease-in-out" : ""} ${
+          showMenu ? "translate-x-[0]" : "-translate-x-full"
+        } absolute left-[0px] top-[0px] flex h-[100vh] w-[250px] basis-[484px] flex-col items-center justify-center gap-[20px]  bg-surface-700 text-[18px] md:static md:mr-auto md:flex md:h-auto md:w-auto md:translate-x-[0] md:flex-row md:bg-transparent md:text-base md:transition-none`}
       >
         <span
           className="absolute right-1 top-1 cursor-pointer md:hidden"
@@ -74,7 +74,7 @@ const Navbar = () => {
                 transition ? "transition-all duration-200 ease-in-out" : ""
               } w-fit border-b-2 border-transparent text-center hover:border-secondary-200 hover:bg-surface-500 hover:text-secondary-200 ${
                 showMenu ? "opacity-100" : "opacity-0"
-              }  md:border-b-0 md:opacity-100 md:transition-none`}
+              } w-full md:border-b-0 md:opacity-100 md:transition-none`}
             >
               <a
                 className="block h-full w-full p-1 md:inline-block md:h-auto md:w-auto md:p-[0px]"
@@ -88,12 +88,12 @@ const Navbar = () => {
             </li>
           );
         })}
-        {/* <li
+        <li
           className={`${
             transition ? "transition-all duration-200 ease-in-out" : ""
-          } w-fit border-b-2 border-transparent text-center hover:border-secondary-200 hover:bg-surface-500 hover:text-secondary-200 ${
+          } ${
             showMenu ? "opacity-100" : "opacity-0"
-          }  md:border-b-0 md:opacity-100 md:transition-none`}
+          } w-fit text-center md:opacity-100 md:transition-none`}
         >
           <a
             className="block rounded-md bg-secondary-600 px-[10px] py-[5px] transition-colors hover:bg-secondary-500 hover:text-white"
@@ -101,7 +101,7 @@ const Navbar = () => {
           >
             Resume
           </a>
-        </li> */}
+        </li>
       </ul>
       <div className="flex basis-[186px] gap-[10px]">
         <div className="flex basis-[64px] items-center gap-[10px]">
