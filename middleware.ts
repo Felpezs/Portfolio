6a1652651fea4e3506c507a7cb90d5cb33beb678
felipe <felipe.edfreire@gmail.com>
2024-04-22 15:29:12 -0300

    fix: rounded image in contact area without bg

--- middleware.ts
+++ middleware.ts
@@ -51,6 +51,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.svg$|).*)",
   ],
 };
